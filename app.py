5f0845840602d567c2e7ad26493d8b8e8864dc2d
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2023-03-29 18:28:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,20 @@ st.set_page_config(
 st.header("🤖 BoardTopic")
 st.subheader("Interactive online topic model and dashboard for BERTopic")
 
-df = pd.read_csv(st.secrets["df"])
-model_load = gdown.download(st.secrets["model"], quiet=False)
-model = BERTopic(model_load)
+@st.cache_data
+def get_df(url):
+    return pd.read_csv(url)
+    
+@st.cache_resource
+def get_model(url):
+    model_load = gdown.download(url, quiet=False)
+    return BERTopic.load(model_load)
+    
+df = get_df(st.secrets["df"])
+model = get_model(st.secrets["model"])
+st.markdown("yay")
 
 topics_over_time = model.topics_over_time(df.original_text, df.date, nr_bins=60,  datetime_format="%d.%m.%Y")
-
+st.markdown("yay2")
 model.visualize_topics_over_time(topics_over_time, top_n_topics=20)
 
